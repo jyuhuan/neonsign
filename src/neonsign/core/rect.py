31d8e61096010b49bbd33859812cc3a5ed598ba2
Yuhuan Jiang <jyuhuan@gmail.com>
2025-08-01 14:21:58 -0700

    Update Rect class

--- src/neonsign/core/rect.py
+++ src/neonsign/core/rect.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from typing import Optional
 
 from neonsign.core.point import Point
 from neonsign.core.size import Size
@@ -27,6 +28,22 @@ class Rect:
             y=self.top_left.y + self.size.height
         )
 
+    @property
+    def left(self) -> int:
+        return self.top_left.x
+
+    @property
+    def right(self) -> int:
+        return self.top_right.x
+
+    @property
+    def top(self) -> int:
+        return self.top_left.y
+
+    @property
+    def bottom(self) -> int:
+        return self.bottom_left.y
+
     def moved_by(self, x_delta: int = 0, y_delta: int = 0) -> Rect:
         if x_delta == 0 and y_delta == 0:
             return self
@@ -38,6 +55,37 @@ class Rect:
             )
         )
 
+    def moved_to_origin(self) -> Rect:
+        return Rect.from_origin(self.size)
+
+    def intersect(self, other: Rect) -> Optional[Rect]:
+        x1 = max(self.top_left.x, other.top_left.x)
+        y1 = max(self.top_left.y, other.top_left.y)
+        x2 = min(
+            self.top_left.x + self.size.width,
+            other.top_left.x + other.size.width
+        )
+        y2 = min(
+            self.top_left.y + self.size.height,
+            other.top_left.y + other.size.height
+        )
+
+        if x2 <= x1 or y2 <= y1:
+            return None
+
+        # Convert to self's coordinate system
+        rel_x1 = x1 - self.top_left.x
+        rel_y1 = y1 - self.top_left.y
+
+        return Rect(
+            top_left=Point(rel_x1, rel_y1),
+            size=Size(width=x2 - x1, height=y2 - y1)
+        )
+
     @classmethod
     def zero(cls) -> Rect:
         return Rect(top_left=Point.origin(), size=Size.zero())
+
+    @classmethod
+    def from_origin(cls, size: Size) -> Rect:
+        return Rect(top_left=Point.origin(), size=size)
--- tests/neonsign/core/test_rect.py
+++ tests/neonsign/core/test_rect.py
@@ -12,6 +12,10 @@ class TestRect(TestCase):
         self.assertEqual(Point(x=100, y=20), r.top_right)
         self.assertEqual(Point(x=10, y=200), r.bottom_left)
         self.assertEqual(Point(x=100, y=200), r.bottom_right)
+        self.assertEqual(10, r.left)
+        self.assertEqual(20, r.top)
+        self.assertEqual(100, r.right)
+        self.assertEqual(200, r.bottom)
 
     def test_transformations(self):
         r = Rect(top_left=Point(x=10, y=20), size=Size(width=90, height=180))
@@ -28,9 +32,46 @@ class TestRect(TestCase):
             Rect(top_left=Point(x=11, y=22), size=Size(width=90, height=180)),
             r.moved_by(x_delta=1, y_delta=2)
         )
+        self.assertEqual(
+            Rect(top_left=Point(x=0, y=0), size=Size(width=90, height=180)),
+            r.moved_to_origin()
+        )
 
     def test_constructors(self):
         self.assertEqual(
             Rect(top_left=Point(x=0, y=0), size=Size(width=0, height=0)),
             Rect.zero()
         )
+        self.assertEqual(
+            Rect(top_left=Point(x=0, y=0), size=Size(width=10, height=20)),
+            Rect.from_origin(Size(width=10, height=20))
+        )
+
+    def test_intersect(self):
+        r0 = Rect(top_left=Point(x=0, y=0), size=Size(width=7, height=5))
+        r1 = Rect(top_left=Point(x=3, y=-1), size=Size(width=5, height=3))
+        r2 = Rect(top_left=Point(x=3, y=3), size=Size(width=5, height=3))
+        r3 = Rect(top_left=Point(x=-1, y=3), size=Size(width=5, height=3))
+        r4 = Rect(top_left=Point(x=-1, y=-1), size=Size(width=5, height=3))
+        r5 = Rect(top_left=Point(x=1, y=1), size=Size(width=5, height=3))
+        r6 = Rect(top_left=Point(x=6, y=1), size=Size(width=5, height=3))
+        r7 = Rect(top_left=Point(x=9, y=1), size=Size(width=5, height=3))
+
+        test_cases = [
+            (r0, r1, Size(width=4, height=2), Point(x=3, y=0), Point(x=0, y=1)),
+            (r0, r2, Size(width=4, height=2), Point(x=3, y=3), Point(x=0, y=0)),
+            (r0, r3, Size(width=4, height=2), Point(x=0, y=3), Point(x=1, y=0)),
+            (r0, r4, Size(width=4, height=2), Point(x=0, y=0), Point(x=1, y=1)),
+            (r0, r5, Size(width=5, height=3), Point(x=1, y=1), Point(x=0, y=0)),
+            (r0, r6, Size(width=1, height=3), Point(x=6, y=1), Point(x=0, y=0)),
+        ]
+
+        for x, y, expected_size, expected_top_left_in_x, expected_top_left_in_y in test_cases:
+            intersect_in_x = x.intersect(y)
+            self.assertEqual(expected_size, intersect_in_x.size)
+            self.assertEqual(expected_top_left_in_x, intersect_in_x.top_left)
+            intersect_in_y = y.intersect(x)
+            self.assertEqual(expected_size, intersect_in_y.size)
+            self.assertEqual(expected_top_left_in_y, intersect_in_y.top_left)
+
+        self.assertIsNone(r0.intersect(r7))
